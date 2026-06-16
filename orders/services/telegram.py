85e5fb2b5e20fe61e2f2d6d3c443a95c3440ca0c
IVal37 <wieran98@gmail.com>
2026-06-16 00:32:02 -0700

    added testing message to telegram.py

--- orders/services/telegram.py
+++ orders/services/telegram.py
@@ -1,2 +1,16 @@
 import asyncio
 from pyrogram import Client
+
+app = Client(
+    "my_account",
+    api_id=38023056,
+    api_hash="e42f8c286585e81dbf8f4dda0f39afb9"
+)
+
+async def main():
+    async with app:
+
+        await app.send_message(chat_id="Test Channel", text="testing")
+
+if __name__ == "__main__":
+    asyncio.run(main())
